447dfdd29ebdb6b1abef0f49c5e14e849defb201
Ruchiket100 <ruchiketborse1@gmail.com>
2022-06-24 10:21:47 +0530

    modified: styles

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,35 +1,20 @@
 import React, { useState } from 'react';
 import Star from './Star';
-import styled from 'styled-components';
 
-const StarRating = ({ totalStars = 5 }) => {
+const StarRating = ({ totalStars = 5, style = {}, ...props }) => {
   const createArray = (number) => [...Array(number)];
   const [selectedStars, setSelectedStars] = useState(3);
   return (
-    <RatingDiv>
-      <div>
-        {createArray(totalStars).map((star, index) => (
-          <Star
-            key={index}
-            selected={selectedStars > index}
-            setSelectedStars={() => setSelectedStars(index + 1)}
-          />
-        ))}
-      </div>
-      <h2>
-        {selectedStars} of {totalStars}
-      </h2>
-    </RatingDiv>
+    <div style={{ padding: 5, ...style }} {...props}>
+      {createArray(totalStars).map((star, index) => (
+        <Star
+          key={index}
+          selected={selectedStars > index}
+          setSelectedStars={() => setSelectedStars(index + 1)}
+        />
+      ))}
+    </div>
   );
 };
-
-const RatingDiv = styled.div`
-  display:flex;
-  flex-direction: column;
-  height: 100vh;
-  align-items: center;
-  justify-content : center;
-  font-size: 5rem;
-`;
 
 export default StarRating;
